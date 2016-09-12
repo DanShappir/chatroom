3a4510d8ce1ec29687ed1624df8c4a780d875ef3
Dan Shappir <dansh@wix.com>
2016-09-12 16:48:25 +0300

    cleanup React

--- public/chat.js
+++ public/chat.js
@@ -4,43 +4,41 @@
 (function () {
     'use strict';
 
-    const render = (msgs, chatClient, root) => ReactDOM.render(<Chat msgs={msgs} send={chatClient.send}/>, root);
+    const render = (msgs, {send}, root) => ReactDOM.render(<Chat msgs={msgs} send={send}/>, root);
 
-    const Chat = ({msgs, send}) =>
+    const Chat = ({msgs, send}) => (
         <div className='chat'>
             <Output msgs={msgs}/>
             <Input send={send}/>
-        </div>;
-    Chat.displayName = 'Chat';
+        </div>
+    );
 
-    const Output = ({msgs}) =>
+    const Output = ({msgs}) => (
         <div className='output'>{
             msgs.map(({from, msg}, i) => <OutputItem key={i} name={from.name} msg={msg}/>)
-            }</div>;
-    Output.displayName = 'Output';
+        }</div>
+    );
 
     const OutputItem = ({name, msg}) => <div><b>{name}:</b> {msg}</div>
-    OutputItem.displayName = 'OutputItem';
 
-    class Input extends React.Component {
-        _send() {
-            const textarea = this.refs.input;
+    const Input = ({send}) => {
+        let textarea;
+        const _send = () => {
             const {value} = textarea;
             textarea.value = '';
-            this.props.send(value);
-        }
-        render() {
-            return <div className='input'>
-                <textarea ref='input'/>
-                <button onClick={this._send.bind(this)}>Send</button>
-            </div>;
-        }
-    }
-    Input.displayName = 'Input';
+            send(value);
+        };
+        return (
+            <div className='input'>
+                <textarea ref={elem => textarea = elem}/>
+                <button onClick={_send}>Send</button>
+            </div>
+        );
+    };
 
     const root = document.getElementById('root');
     const msgs = [];
-    const chatClient = new ChatClient('http://localhost');
+    const chatClient = new ChatClient(location.href);
     chatClient.onConnect = () => {
         chatClient.login('Dan');
         render(msgs, chatClient, root);
